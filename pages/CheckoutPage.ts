8a085f90911d967c3b6a124352cfe15bf55aa766
Sebastien Chipot <chipot.sebastien@gmail.com>
2026-07-12 09:33:22 +0200

    Adding final Checkout.spec.ts

--- pages/CheckoutPage.ts
+++ pages/CheckoutPage.ts
@@ -16,64 +16,78 @@ export class CheckoutPage {
   readonly cancelButton: Locator;
   readonly finishButton: Locator;
   readonly completeHeader: Locator;
-  
 
   constructor(page: Page) {
     this.page = page;
-    this.shoppingCartLink = page.locator('[data-test="shopping-cart-link"]')
-    this.checkoutButton = page.locator('[data-test="checkout"]')
-    this.continueShoppingButton = page.locator('[data-test="continue-shopping"]')
-    this.firstNameInput = page.locator('[data-test="firstName"]')
-    this.lastNameInput = page.locator('[data-test="lastName"]')
-    this.postalCodeInput = page.locator('[data-test="postalCode"]')
-    this.continueButton = page.locator('[data-test="continue"]')
-    this.errorMessage = page.locator('[data-test="error"]')
-    this.subtotalLabel = page.locator('[data-test="subtotal-label"]')
-    this.taxLabel = page.locator('[data-test="tax-label"]')
-    this.totalLabel = page.locator('[data-test="total-label"]')
-    this.cancelButton = page.locator('[data-test="cancel"]')
-    this.finishButton = page.locator('[data-test="finish"]')
-    this.completeHeader = page.locator('[data-test="complete-header"]')
-  }}
+    this.shoppingCartLink = page.locator('[data-test="shopping-cart-link"]');
+    this.checkoutButton = page.locator('[data-test="checkout"]');
+    this.continueShoppingButton = page.locator('[data-test="continue-shopping"]');
+    this.firstNameInput = page.locator('[data-test="firstName"]');
+    this.lastNameInput = page.locator('[data-test="lastName"]');
+    this.postalCodeInput = page.locator('[data-test="postalCode"]');
+    this.continueButton = page.locator('[data-test="continue"]');
+    this.errorMessage = page.locator('[data-test="error"]');
+    this.subtotalLabel = page.locator('[data-test="subtotal-label"]');
+    this.taxLabel = page.locator('[data-test="tax-label"]');
+    this.totalLabel = page.locator('[data-test="total-label"]');
+    this.cancelButton = page.locator('[data-test="cancel"]');
+    this.finishButton = page.locator('[data-test="finish"]');
+    this.completeHeader = page.locator('[data-test="complete-header"]');
+  }
+
   async goToCart() {
     await this.shoppingCartLink.click();
-}
+  }
+
   async goToCheckout() {
     await this.checkoutButton.click();
-}
+  }
+
   async continueCheckout() {
     await this.continueButton.click();
-}
+  }
+
   async cancelCheckout() {
     await this.cancelButton.click();
-}
+  }
+
   async continueShopping() {
     await this.continueShoppingButton.click();
-}
+  }
+
   async finishCheckout() {
     await this.finishButton.click();
-}
+  }
+
   async fillFirstName(value: string) {
     await this.firstNameInput.fill(value);
-}
+  }
+
   async fillLastName(value: string) {
     await this.lastNameInput.fill(value);
-}
+  }
+
   async fillPostalCode(value: string) {
     await this.postalCodeInput.fill(value);
-}
- async getErrorMessage(): Promise<string | null> {
-    return await this.shoppingCartBadge.textContent();
-}
-    async getErrorMessage(): Promise<string | null> {
-    return await this.shoppingCartBadge.textContent();
-}
-    async getErrorMessage(): Promise<string | null> {
-    return await this.shoppingCartBadge.textContent();
-}
-    async getErrorMessage(): Promise<string | null> {
-    return await this.shoppingCartBadge.textContent();
-}
-    async getErrorMessage(): Promise<string | null> {
-    return await this.shoppingCartBadge.textContent();
+  }
+
+  async getErrorMessage(): Promise<string | null> {
+    return await this.errorMessage.textContent();
+  }
+
+  async getSubtotal(): Promise<string | null> {
+    return await this.subtotalLabel.textContent();
+  }
+
+  async getTax(): Promise<string | null> {
+    return await this.taxLabel.textContent();
+  }
+
+  async getTotal(): Promise<string | null> {
+    return await this.totalLabel.textContent();
+  }
+
+  async getCompleteHeader(): Promise<string | null> {
+    return await this.completeHeader.textContent();
+  }
 }
--- tests/Checkout.spec.ts
+++ tests/Checkout.spec.ts
@@ -1,13 +1,12 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/LoginPage';
 import { InventoryPage } from '../pages/InventoryPage';
-import { CheckoutPage } from  '../pages/CheckoutPage';
-
+import { CheckoutPage } from '../pages/CheckoutPage';
 
 test.describe('Checkout', () => {
   let loginPage: LoginPage;
   let inventoryPage: InventoryPage;
-  let checkoutPage : CheckoutPage;
+  let checkoutPage: CheckoutPage;
 
   test.beforeEach(async ({ page }) => {
     loginPage = new LoginPage(page);
@@ -31,6 +30,7 @@ test.describe('Checkout', () => {
     await checkoutPage.continueCheckout();
     expect(await checkoutPage.getErrorMessage()).toBe('Error: Last Name is required');
   });
+
   test('Postal code missing', async ({ page }) => {
     await checkoutPage.goToCart();
     await checkoutPage.goToCheckout();
@@ -39,15 +39,71 @@ test.describe('Checkout', () => {
     await checkoutPage.continueCheckout();
     expect(await checkoutPage.getErrorMessage()).toBe('Error: Postal Code is required');
   });
-  test('Cancelling Checkout', async ({ page }) => {
+
+  test('Cancelling checkout', async ({ page }) => {
     await inventoryPage.addToCart('sauce-labs-backpack');
     await checkoutPage.goToCart();
     await checkoutPage.goToCheckout();
-    await checkoutPage.fillFirstName('Seb');
-    await checkoutPage.fillLastName('Chup');
+    await checkoutPage.fillFirstName('Sebastien');
+    await checkoutPage.fillLastName('chup');
     await checkoutPage.fillPostalCode('60300');
     await checkoutPage.continueCheckout();
-    await checkoutPage.cancelCheckout()
+    await checkoutPage.cancelCheckout();
     await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
     expect(await inventoryPage.getBadgeCount()).toBe('1');
   });
+
+  test('Checking total price', async ({ page }) => {
+    await inventoryPage.addToCart('sauce-labs-backpack');
+    await checkoutPage.goToCart();
+    await checkoutPage.goToCheckout();
+    await checkoutPage.fillFirstName('Sebastien');
+    await checkoutPage.fillLastName('chup');
+    await checkoutPage.fillPostalCode('60300');
+    await checkoutPage.continueCheckout();
+
+    const subtotalText = await checkoutPage.getSubtotal();
+    const subtotal = parseFloat(subtotalText!.split('$')[1]);
+    const taxText = await checkoutPage.getTax();
+    const tax = parseFloat(taxText!.split('$')[1]);
+    const totalText = await checkoutPage.getTotal();
+    const total = parseFloat(totalText!.split('$')[1]);
+
+    expect(subtotal + tax).toBeCloseTo(total, 2);
+  });
+
+  test('Order completed', async ({ page }) => {
+    await inventoryPage.addToCart('sauce-labs-backpack');
+    await checkoutPage.goToCart();
+    await checkoutPage.goToCheckout();
+    await checkoutPage.fillFirstName('Sebastien');
+    await checkoutPage.fillLastName('chup');
+    await checkoutPage.fillPostalCode('60300');
+    await checkoutPage.continueCheckout();
+    expect(await checkoutPage.getTotal()).toContain('Total:');
+    await checkoutPage.finishCheckout();
+    expect(await checkoutPage.getCompleteHeader()).toBe('Thank you for your order!');
+  });
+});
+
+test.describe('Continue Shopping', () => {
+  let loginPage: LoginPage;
+  let inventoryPage: InventoryPage;
+  let checkoutPage: CheckoutPage;
+
+  test.beforeEach(async ({ page }) => {
+    loginPage = new LoginPage(page);
+    inventoryPage = new InventoryPage(page);
+    checkoutPage = new CheckoutPage(page);
+    await loginPage.goto();
+    await loginPage.login('standard_user', 'secret_sauce');
+  });
+
+  test('Go back to Shopping items list', async ({ page }) => {
+    await inventoryPage.addToCart('sauce-labs-backpack');
+    await checkoutPage.goToCart();
+    await checkoutPage.continueShopping();
+    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+    await expect(page.locator('[data-test="title"]')).toHaveText('Products');
+  });
+});
